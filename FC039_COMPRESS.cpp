cb60a5c5d81c9dfce9cf2ee7fb6158b464bb8e26
Tri2k6 <cheatgaming647@gmail.com>
2025-02-22 20:12:58 +0700

    Adding File FC039_COMPRESS.cpp

---
+++ FC039_COMPRESS.cpp
@@ -0,0 +1,158 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+template<class integer>
+inline integer to_int(const string& s, size_t* idx = 0, int base = 10) {
+    size_t n = s.size(), i = idx ? *idx : 0; bool sign = false; integer x = 0;
+    if (s[i] == '-')
+        ++i, sign = true;
+    function<int(char)> char_to_digit = [&](char c) {
+        static const int d[] = {'a'-10,'0'}; 
+        return tolower(c)-d[isdigit(c)]; };
+    while (i < n)
+        x *= base, x += char_to_digit(s[i++]);
+    if (idx)
+        *idx = i; 
+    return sign ? -x : x; }
+ 
+template<class integer>
+inline string to_string(integer x, int base = 10) {
+    bool sign = false; string s;
+    if (x < 0)
+        x = -x, sign = true;
+    function<char(int)> digit_to_char = [](int d) {
+        static const char c[] = {'a'-10,'0'};
+        return c[d < 10]+d; };
+    do 
+        s += digit_to_char(x%base), x /= base;
+    while (x > 0); 
+    if (sign)
+        s += '-';
+    reverse(s.begin(),s.end());
+    return s; }
+ 
+template<class integer>
+inline istream& read(istream& is, integer& x) {
+    string s; is >> s, x = to_int<integer>(s); return is; }
+ 
+template<class integer>
+inline ostream& write(ostream& os, integer x) { return os << to_string(x); }
+ 
+using  lll =   signed __int128; 
+using ulll = unsigned __int128;
+ 
+inline istream& operator>>(istream& is,  lll &x) { return  read(is,x); }
+inline istream& operator>>(istream& is, ulll &x) { return  read(is,x); }
+inline ostream& operator<<(ostream& os,  lll  x) { return write(os,x); }
+inline ostream& operator<<(ostream& os, ulll  x) { return write(os,x); }
+
+#define input cin
+#define output cout
+#define ll long long 
+#define ull unsigned long long
+#define pii pair<int,int>
+#define endl '\n'
+#define all(v) v.begin(),v.end()
+#define sorta(v) sort(v.begin(),v.end())
+#define mem(a,b) memset(a,b,sizeof(a));
+#define GetBit(u) (u & -u)
+#define bit(u,i) ((u >> i) & 1)
+#define mask(i) (1ll << i)
+#define vi vector<int>
+#define int128 __int128
+#define fi first
+#define se second
+#define heap_max(a) priority_queue<a>
+#define heap_min(a) priority_queue<a, vector<a>, greater <a>>
+#define pb push_back
+#define eb emplace_back
+#define TASK "TASK"
+
+const int cs = 1e6 + 7;
+const int mod = 1e9 + 7;
+const int INF = mod;
+const int maxN = 2e3 + 7;
+const int block_size = 320;
+const ll oo = 1e18 + 7;
+
+template<class X, class Y>
+    bool minimize(X &x, const Y &y) {
+        if (x > y) {
+            x = y;
+            return true;
+        } else return false;
+    }
+
+template<class X, class Y>
+    bool maximize(X &x, const Y &y) {
+        if (x < y) {
+            x = y;
+            return true;
+        } else return false;
+    }
+
+template<class T>
+    T Abs(const T &x) {
+        return (x < 0 ? -x : x);
+    }
+
+template<class X, class Y>
+    void Add(X &x , const Y &y) {
+        x += y;
+        if (x > mod) x -= mod;
+    }
+
+template<class X, class Y>
+    void Sub(X &x, const Y &y) {
+        x -= y;
+        if (x < 0) x += mod;
+    }
+
+// 
+
+string str;
+int n;
+
+void nhap() {
+    getline(input,str);
+    n = str.length();
+    str = '0' + str;
+}
+
+bool isCheck(string str,string s,int k) {
+    string tmp = "";
+    for (int i = 1;i <= k;i++) {
+        tmp += s;
+    }
+
+    return str.substr(1, n) == tmp;
+}
+
+void Solve() {
+    int ans = 1;
+
+    for (int i = 1;i * i <= n;i++) {
+        if (n % i == 0) {
+            if (isCheck(str, str.substr(1, i), n / i)) maximize(ans, n / i);
+            if (isCheck(str, str.substr(1, n / i), i)) maximize(ans, i);
+        }
+    }
+    output << ans << str.substr(1, n / ans);
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    if(fopen(TASK".inp", "r")) {
+        freopen(TASK".inp", "r", stdin);
+        freopen(TASK".out", "w", stdout);
+    }
+    int T = 1;
+    // input >> T;
+    while (T--) {
+        nhap();
+        Solve();
+    }
+    return 0;
+}
